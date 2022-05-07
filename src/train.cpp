3cb60d0a78090195055aca4feec1d60a295a5531
evgenii-034 <99119985+evgenii-034@users.noreply.github.com>
2022-05-07 11:24:42 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,50 +1,53 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-Train::Train() : countOp(0), first(nullptr) {}
+Train::Train(): countOp(0), first(nullptr) {}
 void Train::addCage(bool light) {
-  Cage *rwc = new Cage;
-  rwc->light = light;
-  rwc->next = nullptr;
-  rwc->prev = nullptr;
-  if (first == nullptr) {
-    first = rwc;
-  } else if (first->next == nullptr) {
-    first->next = rwc;
-    rwc->prev = first;
-    first->prev = rwc;
-    rwc->next = first;
-  } else {
-      first->prev->next = rwc;
-      rwc->prev = first->prev;
-      first->prev = rwc;
-      rwc->next = first;
-  }
+if (first != nullptr) {
+Cage* ncage;
+ncage = new Cage;
+ncage->light = light;
+ncage->next = nullptr;
+ncage->prev = last;
+last->next = ncage;
+last = ncage;
+} else {
+first = new Cage;
+first->light = light;
+first->next = nullptr;
+first->prev = nullptr;
+last = first;
+return;
+}
 }
 int Train::getLength() {
-Cage* cr = first;
-  first->light = true;
-  countOp = 0;
-  length = 0;
-  while (true) {
-    int trlen;
-    countOp++;
-    length++;
-    cr = cr->next;
-    if (cr->light) {
-      cr->light = false;
-      trlen = length;
-      for (trlen; trlen > 0; trlen--) {
-        cr = cr->prev;
-        countOp++;
-      }
-      if (!cr->light) {
-        return length;
-      }
-      length = trlen;
-      length = trlen;
-    }
-  }
+first->light = true;
+first->prev = last;
+last->next = first;
+Cage* temp = first;
+Cage* n = first->next;
+while (n != nullptr) {
+if (!n->light) {
+count++;
+countOp++;
+n = n->next;
+} else {
+if (n->light) {
+n->light = 0;
+countOp++;
+}
+for (int i = count; i > 0; i--) {
+n = n->prev;
+countOp++;
+}
+if (!n->light) {
+break;
+}
+n = n->next;
+count = 1;
+}
+}
+return count;
 }
 int Train::getOpCount() {
-  return countOp;
+return countOp;
 }
